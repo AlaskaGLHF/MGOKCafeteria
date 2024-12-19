74dcb451e9dd9cab6e2d0911641a9cf7a8183970
AlaskaGLHF <vanushaa89@gmail.com>
2024-12-19 18:30:00 +0300

    black theme without, all work

--- app/src/main/java/com/example/mgokcafeteria/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mgokcafeteria/ui/theme/Theme.kt
@@ -1,47 +1,33 @@
 package com.example.mgokcafeteria.ui.theme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+private val LightColors = lightColorScheme(
+    primary = Color(0xFF6200EE),
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Color(0xFFF5F5F5),
+    onBackground = Color.Black
+)
 
+private val DarkColors = darkColorScheme(
+    primary = Color(0xFFBB86FC),
+    onPrimary = Color.Black,
+    background = Color(0xFF121212),
+    onBackground = Color.White
+)
 
 @Composable
-fun MGOKCafeteriaTheme(darkTheme: Boolean = false, content: @Composable () -> Unit) {
-    val colorScheme = if (darkTheme) {
-        darkColorScheme(
-            primary = Color(0xFFBB86FC),
-            onPrimary = Color.Black,
-            background = Color.Black,
-            surface = Color.DarkGray,
-            onBackground = Color.White,
-            onSurface = Color.White
-        )
-    } else {
-        lightColorScheme(
-            primary = Color(0xFF6200EE),
-            onPrimary = Color.White,
-            background = Color.White,
-            surface = Color.LightGray,
-            onBackground = Color.Black,
-            onSurface = Color.Black
-        )
-    }
+fun MGOKCafeteriaTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
